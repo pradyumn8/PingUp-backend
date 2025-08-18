3be6557981cc20e841a3e5a6743d298642983eb9
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-08-18 11:32:25 +0530

    Message routes created

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -6,7 +6,7 @@ import Message from '../models/Message.js';
 const connections = {};
 
 // Controller function for the SSE endpoint
-export const addUserStory = async (req, res) => {
+export const sseController = async (req, res) => {
     const { userId } = req.params
     console.log('New client connected:', userId);
 
@@ -77,6 +77,44 @@ export const sendMessage = async (message) => {
 
     } catch (error) {
         console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+// Get Chat Messages
+export const getChatMessages = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { to_user_id } = req.body;
+
+        const messages = await Message.find({
+            $or: [
+                { from_user_id: userId, to_user_id },
+                { from_user_id: to_user_id, to_user_id: userId }
+            ]
+        }).sort({ createdAt: -1 })
+        // Mark messges as seen
+        await Message.updateMany(
+            { from_user_id: to_user_id, to_user_id: userId, seen: false },
+            { $set: { seen: true } }
+        );
+
+        res.json({ success: true, messages });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export const getUserRecentMessages = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const messages = await Message.find({ to_user_id: userId }
+            .populate('from_user_id to_user_id')).sort({ createdAt: -1 });
+
+        res.json({ success: true, messages });
+    } catch (error) {
         res.json({ success: false, message: error.message });
     }
 }
